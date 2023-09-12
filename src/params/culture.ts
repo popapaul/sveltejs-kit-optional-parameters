9dbe2c70329875d5fec4d29a9ca11f133e4ab7c2
popapaul <39795380+popapaul@users.noreply.github.com>
2023-09-12 11:34:37 +0300

    2 files modified

--- src/params/culture.ts
+++ src/params/culture.ts
@@ -3,7 +3,7 @@ export const match: ParamMatcher = (param) => {
 	return param.length === 2;
 };
 
-export default 'en';
+//export default 'en';
 
 // Perhaps a matcher could also return a default value when it is not matched;
 // This maybe would only make sense in case of optional parameters;
--- src/params/year.ts
+++ src/params/year.ts
@@ -4,7 +4,7 @@ export const match: ParamMatcher = (param) => {
 	return param.length == 4 && Number(param) > 2000 && Number(param) <= currentYear;
 };
 
-export default currentYear.toString();
+//export default currentYear.toString();
 
 // Perhaps a matcher could also return a default value when it is not matched;
 // This maybe would only make sense in case of optional parameters;
